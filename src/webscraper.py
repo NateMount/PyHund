8be3d058adc3e244e369dba0965c8dcbb93963fd
Nate Mount <ngmount2021@gmail.com>
2022-02-23 11:51:21 -0500

    Removed validation method from webscraper

--- src/webscraper.py
+++ src/webscraper.py
@@ -2,7 +2,6 @@
 
 import json
 import requests as r
-from bs4 import BeautifulSoup
 from src.util import Log
 
 #TODO remove after no longer needed
@@ -37,23 +36,3 @@ def get(site_name:str, username:str):
 		return r.get(site_data["url"].format(username), headers=site_head)
 	except r.ConnectionError:
 		return None
-
-
-def verify(site_name, site) -> bool:
-	"""Used to verify if a site is a valid user response"""
-    
-	Log(site_name, site, sep="\n")
-
-	#Reformatting site info for more precise data
-	site_data = get_site(site_name)
-	
-	soup = BeautifulSoup(site.txt, 'html.parser')
-	
-
-	is_invalid = False
-
-	for check in site_data["invalid"]:
-		if check in site_text:
-			is_invalid = True
-
-	return (site.status_code == 200) and (site_data["valid"] in site_text and not is_invalid)
