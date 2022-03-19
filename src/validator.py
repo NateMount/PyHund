4e535e20c68e9c6ba1a0d7fc4dba535705a71caf
Nate Mount <ngmount2021@gmail.com>
2022-03-19 17:28:45 -0400

    Updated validator to python3.10 match case

--- src/validator.py
+++ src/validator.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3
+#! /usr/bin/env python3.10
 
 import re
 from . import webscraper
@@ -7,14 +7,23 @@ def verify(site_name:str, site) -> bool:
 	"""Used to verify if a site is a valid user response"""
 	
 	site_data:dict = webscraper.get_site(site_name)
-	
-	if site_data['verify-method'] == 'status':
-		if 'verify-str' in site_data:
-			return site.status_code == int(site_data['verify-str'])
-		return site.status_code == 200
-	elif site_data['verify-method'] == 'url':
-		return site_data['valid-str'] in site.url
-	elif site_data['verify-method'] == 'match':
-		return re.search(site_data['verify-str'], site.text) != None
-	else:
+
+	if not site_data:
 		return False
+
+	match site_data['verify-method']:
+		case 'status':
+			if 'verify-str' in site_data:
+				return site.status_code == int(site_data['verify-str'])
+			else:
+				return site.status_code == 200
+
+		case 'url':
+			return site_data['valid-str'] in site.url
+
+		case 'match':
+			return re.search(site_data['verify-method'], site.text) != None
+
+		case _:
+			return False
+
