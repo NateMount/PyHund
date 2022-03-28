a0b85763a291028f56369099e463d34ad3da2bec
Nate Mount <ngmount2021@gmail.com>
2022-03-28 10:53:35 -0400

    Updated syntax

--- src/validator.py
+++ src/validator.py
@@ -6,23 +6,23 @@ from . import webscraper
 def verify(site_name:str, site) -> bool:
 	"""Used to verify if a site is a valid user response"""
 	
-	site_data:dict = webscraper.get_site(site_name)
+	_data:dict = webscraper.get_site(site_name)
 
-	if not site_data:
+	if not _data:
 		return False
 
 	match site_data['verify-method']:
 		case 'status':
-			if 'verify-str' in site_data:
-				return site.status_code == int(site_data['verify-str'])
+			if 'verify-str' in _data:
+				return site.status_code == int(_data['verify-str'])
 			else:
 				return site.status_code == 200
 
 		case 'url':
-			return site_data['verify-str'] in site.url
+			return _data['verify-str'] in site.url
 
 		case 'match':
-			return re.search(site_data['verify-method'], site.text) != None
+			return re.search(_data['verify-method'], site.text) != None
 
 		case _:
 			return False
